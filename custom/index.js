f1e2b3d91443e22ace5dd2c049c5a6403c3b7385
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-17 19:48:33 +0530

    improve optimization

--- custom/index.js
+++ custom/index.js
@@ -10,15 +10,14 @@ const fetchData = async () => {
 fetchData();
 
 const ConstructBlogs = ({ blogs }) => {
-  // console.log([blogs]);
   const serializedBlogs = serializer(blogs);
 
   serializedBlogs.forEach((blog, index) => {
     blogSection.innerHTML += `
      <div class="col-lg-4 col-md-6">
                     <div class="news-info">
-                        <figure class="news-img"><a href="./blog-detail.html?id=${blog.id}"><img src="${blog.imageUrl}"
-                                    alt="news imag" class="blog-image"></a>
+                        <figure class="news-img"><a href="./blog-detail.html?id=${blog.id}"><img data-src="${blog.imageUrl}"
+                                    alt="news image" class="blog-image lazyimg"></a>
                         </figure>
                         <div class="detail">
                             <label>${blog.createdAt} <a href="./blog-detail.html?id=${blog.id}">Business</a></label>
@@ -32,6 +31,7 @@ const ConstructBlogs = ({ blogs }) => {
                 </div>
     `;
   });
+  lazyLoadImages();
 };
 
 const serializer = (data) => {
@@ -50,7 +50,7 @@ const ProjectData = async () => {
         <div class="news-info">
         
             <figure class="news-img">
-  <a target="_blank" href="${project.ProjectLink}"><img src="${project.imageUrl}"alt="news imag" class="blog-image"></a>
+  <a target="_blank" href="${project.ProjectLink}"><img  data-src="${project.imageUrl}"alt="news imag" class="blog-image lazyimg"></a>
             </figure>
 
             <div class="detail">
@@ -61,6 +61,28 @@ const ProjectData = async () => {
     </div>
         `;
   });
+
+  lazyLoadImages();
 };
 
 ProjectData();
+
+const lazyLoadImages = () => {
+  const images = document.querySelectorAll(".lazyimg");
+  images.forEach((image) => {
+    if (!image.getAttribute("data-loaded")) {
+      const observer = new IntersectionObserver((entries) => {
+        image.setAttribute("data-loaded", "true");
+
+
+        if (entries[0].isIntersecting) {
+          image.src = image.getAttribute("data-src");
+          observer.disconnect();
+        }
+      },{rootMargin:'200px'});
+
+      observer.observe(image);
+    }
+  });
+};
+lazyLoadImages();
